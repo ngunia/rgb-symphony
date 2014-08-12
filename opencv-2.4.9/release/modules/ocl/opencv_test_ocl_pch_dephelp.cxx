5ed114fdaf2a41a5002329c218fe1536c8f13f3b
Nick <njg39@drexel.edu>
2014-08-12 12:37:49 -0400

    removed opencv install files

--- opencv-2.4.9/release/modules/ocl/opencv_test_ocl_pch_dephelp.cxx
+++
@@ -1,6 +0,0 @@
-#include "/home/nick/workspace/rgb-symphony/opencv-2.4.9/modules/ocl/test/test_precomp.hpp"
-int testfunction();
-int testfunction()
-{
-    return 0;
-}
--- opencv-2.4.9/release/modules/stitching/opencv_perf_stitching_pch_dephelp.cxx
+++
@@ -1,6 +0,0 @@
-#include "/home/nick/workspace/rgb-symphony/opencv-2.4.9/modules/stitching/perf/perf_precomp.hpp"
-int testfunction();
-int testfunction()
-{
-    return 0;
-}
